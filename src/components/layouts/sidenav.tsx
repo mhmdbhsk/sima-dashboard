b5bd6ff106090c82bf006ac0be2fbfdfceaaf86e
mhmdbhsk <muhammadbhaska0@gmail.com>
2023-10-30 07:51:53 +0700

    feat: change default app name

--- src/components/layouts/sidenav.tsx
+++ src/components/layouts/sidenav.tsx
@@ -15,7 +15,7 @@ export default function SideNav() {
   return (
     <div className='flex h-full flex-col px-3 py-2 md:px-2'>
       <Link className='mb-2 flex h1-15 items-end justify-start rounded-md bg-primary p-4 md:h-20' href='/'>
-        <div className='w-32 text-white md:w-40'>warung.io</div>
+        <div className='w-32 text-white md:w-40'>dashboard</div>
       </Link>
       <div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
         <NavLinks />
